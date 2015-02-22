83140d56acab53a2df2af125bccbced8b6e7fcdc
Francesco Trotta <frtrotta@users.noreply.github.com>
2015-02-22 11:50:30 +0100

    Completato

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ese011-InputArgsExitCode
+
+Programma che mostra l'utilizzo di argomenti per l'invocazione del programma
+(input arguments) e del un codice di uscita (exit code).
---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char* argv[]) {
+    printf("Il nome del file eseguibile è \"%s\"\n\n", argv[0]);
+    
+    if(argc > 1) {
+        int i;    
+        for(i=1; i<argc; i++) {
+            printf("L'argomento %d e': \"%s\"\n", i, argv[i]);
+        }
+    }
+    else {
+        puts("Hai invocato il programma senza alcun argomento.");
+        puts("Riprova, specificando gli argomenti dopo il nome del programma, separandoli con uno spazio.");
+    }
+
+    srand(time(NULL));
+    int r = rand() % 255;
+    printf("\nHo generato il numero casuale %d: lo utilizzo come exit code.\n", r);
+    
+    return r;
+}
